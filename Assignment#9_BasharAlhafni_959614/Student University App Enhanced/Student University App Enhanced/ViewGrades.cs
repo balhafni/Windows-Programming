46184a64c2b293d12ba28cd3959516bc80ed4b13
Bashar Alhafni <balhafni@my.bridgeport.edu>
2018-01-21 21:26:14 -0500

    Windows programming v0.1

---
+++ Assignment#9_BasharAlhafni_959614/Student University App Enhanced/Student University App Enhanced/ViewGrades.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Student_University_App_Enhanced.Models;
+using Student_University_App_Enhanced.BusinessLayer;
+namespace Student_University_App_Enhanced
+{
+    public partial class ViewGrades : Form
+    {
+        IBusinessStudent _ibusinessStudent = null;
+        public ViewGrades()
+        {
+            InitializeComponent();
+            _ibusinessStudent = new Business() as IBusinessStudent;
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            List<CoursesTaken> cList = null;
+            try
+            {
+                cList = _ibusinessStudent.GetGradeByStudentId(txtId.Text);
+                if(cList.Count == 0)
+                {
+                    MessageBox.Show("Invalid Id");
+                    return;
+                }
+                dataGrid.DataSource = cList;
+                dataGrid.Refresh();
+
+
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
